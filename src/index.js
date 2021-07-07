6af8b8e15f24180908f84b789b4bbf5e9a7b8a5f
Aibinuomo Ifemide Babatope <aib.ifemide@gmail.com>
2021-07-07 10:43:38 +0100

    user profile done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import App from './pages/App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import User from './User'
+import User from './pages/User'
 
 ReactDOM.render(
   <React.StrictMode>
